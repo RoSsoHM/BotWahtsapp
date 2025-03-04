4519daa207ae3e23154d27ff01235b7198992cf5
RoSso HM <83230417+RoSsoHM@users.noreply.github.com>
2025-03-04 17:40:39 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Netero',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Netero Bot",
+  botOwner: 'RoSsoHM', // Your name
+  ownerNumber: '212658440954', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
